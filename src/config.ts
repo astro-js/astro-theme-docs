withastro/astro

WIP update examples/docs/

examples/docs/src/config.ts
@@ -1,10 +1,33 @@
-export const sidebar = [
-  {
-    text: 'Introduction',
-    link: '', // No leading slash needed, so this links to the homepage
-    children: [
-      { text: 'Getting Started', link: 'getting-started' },
-      { text: 'Example', link: 'example' },
-    ],
+export const SIDEBAR = {
+  en: [
+    { text: 'Introduction', link: 'introduction', header: true },
+    { text: 'Getting Started', link: 'getting-started' },
+    { text: 'Example', link: 'example' },
+  ],
+  es: [
+    { text: 'Introducción', link: 'es/introduction', header: true },
+    { text: 'Empezando', link: 'es/getting-started' },
+    { text: 'Ejemplo', link: 'es/example' },
+  ],
+  fr: [
+    { text: 'Introduction', link: 'fr/introduction', header: true },
+    { text: 'Commencer', link: 'fr/getting-started' },
+    { text: 'Exemple', link: 'fr/example' },
+  ],
+};
+
+export const SITE = {
+  title: 'Astro Documentation',
+  description: 'Build faster websites with less client-side Javascript.',
+};
+
+export const OPEN_GRAPH = {
+  locale: 'en_US',
+  image: {
+    src: 'https://github.com/snowpackjs/astro/blob/main/assets/social/banner.png?raw=true',
+    alt:
+      'astro logo on a starry expanse of space,' +
+      ' with a purple saturn-like planet floating in the right foreground',
   },
-];
+  twitter: 'astrodotbuild',
+};

astro-js/astro-theme-docs

src/config.ts
@@ -1,10 +1,22 @@
-export const sidebar = [
-  {
-    text: 'Introduction',
-    link: '', // No leading slash needed, so this links to the homepage
-    children: [
-      { text: 'Getting Started', link: 'getting-started' },
-      { text: 'Example', link: 'example' },
-    ],
+export const SIDEBAR = {
+  en: [
+    { text: 'Getting Started', header: true },
+    { text: 'Introduction', link: 'en/introduction' },
+    { text: 'Getting Started', link: 'en/getting-started' },
+    { text: 'Example', link: 'en/example' },
+  ],
+};
+
+export const SITE = {
+  title: 'Astro Documentation',
+  description: 'Build faster websites with less client-side Javascript.',
+};
+
+export const OPEN_GRAPH = {
+  locale: 'en_US',
+  image: {
+    src: 'https://github.com/snowpackjs/astro/blob/main/assets/social/banner.png?raw=true',
+    alt: 'astro logo on a starry expanse of space,' + ' with a purple saturn-like planet floating in the right foreground',
   },
-];
+  twitter: 'astrodotbuild',
+};